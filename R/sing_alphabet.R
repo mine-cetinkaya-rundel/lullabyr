cc77927d1635bffe3aba73bca7aac3aab7e697af
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2018-11-23 22:52:56 -0500

    Document sing_alphabet function

--- R/sing_alphabet.R
+++ R/sing_alphabet.R
@@ -1,55 +1,54 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
-sing_alphabet <- function(wordbag, fix){
+#' Sing the alphabet song
+#'
+#' Create the alphabet song by randomly sampling words with the appropriate
+#' initials from the wordbag provided.
+#'
+#' @param wordbag A character vector of words to sample from.
+#' @importFrom dplyr mutate
+#' @importFrom dplyr arrange
+#' @importFrom dplyr group_by
+#' @importFrom dplyr slice
+#' @importFrom dplyr select
+#'
+#' @export
+#' @examples
+#'
+#' sing_alphabet(fruits)
+#' sing_alphabet(animals)
+#' sing_alphabet(vegetables)
+#' sing_alphabet(countries)
+#'
+sing_alphabet <- function(wordbag){
 
   if(missing(wordbag)){
     stop("Please provide a wordbag.")
   }
 
-  if(!missing(fix)){
-    fixed_words <- tibble(
-      word = fix,
-      initial = tolower(str_sub(fix, 1, 1))
-    )
-  } else{
-    fixed_words <- tibble(
-      fix = character(),
-      initial = character()
-    )
+  if(length(wordbag) == 0){
+    stop("Please provide a wordbag with at least one word.")
   }
 
-  selected_words_wo_fixed <- wordbag %>%
-    mutate(initial = str_sub(word, 1, 1)) %>%
-    filter(!(initial %in% fixed_words$initial)) %>%
+  words_df <- data.frame(
+    word = wordbag
+  )
+
+  selected_words_df <- words_df %>%
+    mutate(initial = toupper(str_sub(word, 1, 1))) %>%
     mutate(rando = runif(n())) %>%
     arrange(initial, rando) %>%
     group_by(initial) %>%
     slice(1) %>%
     select(-rando)
 
-  selected_words <- full_join(selected_words_wo_fixed, fixed_words) %>%
-    arrange(initial) %>%
-    mutate(upper_initial = toupper(initial)) %>%
-    arrange(initial)
+  if(nrow(selected_words_df) < 26){
+    warning("Your wordbag does not contain at least one word for each letter in the English alphabet, some letters will be skipped.")
+  }
 
   cat(
     paste0(
-      selected_words$upper_initial, " is for ", selected_words$word, ", ",
-      selected_words$upper_initial, " - ", selected_words$initial, " - ",
-      selected_words$word, ".\n"
+      selected_words_df$initial, " is for ", selected_words_df$word, ", ",
+      selected_words_df$initial, " - ", selected_words_df$initial, " - ",
+      selected_words_df$word, ".\n"
     )
   )
 
