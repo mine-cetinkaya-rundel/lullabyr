173c0e05878bc9375f715e88c0bef5f95a0e9990
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2018-11-24 00:08:28 -0500

    Add vegs with missing letters

--- data-raw/vegetables.R
+++ data-raw/vegetables.R
@@ -12,5 +12,13 @@ vegetables <- vegetables_page %>%
   html_text() %>%
   toTitleCase()
 
+# add vegetables starting with I, U, X, V, Z
+# ref I, U, Z: https://edis.ifas.ufl.edu/features/fruitvegindex.html
+# ref X: http://food.answers.com/Q/Is_a_xerophyte_a_fruit_or_vegetable
+# ref V: https://answers.yahoo.com/question/index?qid=20100309004707AALUTZU
+
+vegetables <- c(vegetables, "Ice Plant", "Upland Cress", "Xerophyte",
+                "Velvet Bean", "Vidalia Onion", "Zucchini")
+
 # save data --------------------------------------------------------------------
 use_data(vegetables)
